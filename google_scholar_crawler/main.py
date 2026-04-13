18ac5d8a61b03d1c98cddd241990746179fb0a94
Moraldeepsingh Sachdeo <46534341+moraldeep@users.noreply.github.com>
2026-04-13 12:07:48 -0700

    update new

---
+++ google_scholar_crawler/main.py
@@ -0,0 +1,22 @@
+from scholarly import scholarly
+import json
+from datetime import datetime
+import os
+
+author: dict = scholarly.search_author_id('12dLp4wAAAAJ')
+scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+name = author['name']
+author['updated'] = str(datetime.now())
+author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+print(json.dumps(author, indent=2))
+os.makedirs('results', exist_ok=True)
+with open('results/gs_data.json', 'w') as outfile:
+    json.dump(author, outfile, ensure_ascii=False)
+
+shieldio_data = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['citedby']}",
+}
+with open('results/gs_data_shieldsio.json', 'w') as outfile:
+    json.dump(shieldio_data, outfile, ensure_ascii=False)
